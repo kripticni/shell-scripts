f7a1362e5e4cf7c28169b2ac4061911602a53b85
kripticni <kripticno@gmail.com>
2024-05-12 12:58:04 +0200

    Simply extending and improving on the existing scripts.

--- extramonitor.sh
+++ extramonitor.sh
@@ -1,11 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 intern="eDP-1"
 extern="HDMI-1"
-case "$1" in
+choice=$1
+if [[ -z $choice ]]; then
+	choice=$(printf "disconnect\nconnect\nduplicate" | fzf)
+fi
+
+case "$choice" in
 	"disconnect") xrandr --output "$intern" --off && xrandr --output "$extern" --mode 1920x1080;;
 	"connect") xrandr --output "$intern" --auto --right-of "$extern";;
 	"duplicate") xrandr --output "$extern" --auto --same-as "$intern" --mode 1920x1080 && xrandr --output "$intern" --mode 1920x1080;;
-	*) echo "its disconnect/connect/duplicate" ;;
 esac
 ~/.fehbg
 
--- fastc.sh
+++ fastc.sh
@@ -1,8 +1,20 @@
-#!/bin/bash
-source=$(cat $HOME/.cache/fastc.save)
-case "$1" in
-	"build") gcc -Wall -o C $source && ./C ;;
-	"config") echo $2 > $HOME/.cache/fastc.save && source=$2 && gcc -Wall -o C $source && ./C ;;
-	"debug") gcc -Wall -g -o G $source && gdb G;; 
-	*) echo "build, or config sourcefile" ;;
+#!/usr/bin/env bash
+
+source=$(cat $HOME/.cache/fastc.save | awk -F '\t' '$1 == $(pwd) {print $2}')
+arg=$1
+
+case "$arg" in
+	"compile") gcc -Wall -o C $source && ./C ;;
+	"debug") gcc -Wall -g -o G $source && gdb G;;
+	"config") sed -i '/$(pwd)/d' $HOME/.cache/fastc.save
+		printf "%s\t%s" $(pwd) $2 > $HOME/.cache/fastc.save
+		gcc -Wall -o C $2 && ./C;;
+	*) echo "Usage: fastc.sh [OPTION] [SOURCEFILE]
+                A simple script for faster C script management.
+
+                Options:
+                        compile: NULL
+                        debug: NULL
+                        config: SOURCEFILE
+                        ";;
 esac
