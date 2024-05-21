70132cb3ea02a7fd93a5dcd7db6ecdde7119289d
kripticni <kripticno@gmail.com>
2024-05-21 13:31:04 +0200

    Learnt a fair bit more about shell scripting, came back to patch.

--- fastc.sh
+++ fastc.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
-source=$(cat $HOME/.cache/fastc.save | awk -F '\t' '$1 == $(pwd) {print $2}')
-arg=$1
+source=$(cat $HOME/.cache/fastc.save | awk -F '\t' -v dir=$(pwd) '$1 == dir {print $2}')
+save=$2
+if [ -z "$source" ] && [ "$1" != "config" ]; then
+  save=$(ls | xargs | fzf)
+  arg="config"
+else
+  arg=$1
+fi
 
 case "$arg" in
 	"compile") gcc -Wall -o C $source && ./C ;;
 	"debug") gcc -Wall -g -o G $source && gdb G;;
-	"config") sed -i '/$(pwd)/d' $HOME/.cache/fastc.save
-		printf "%s\t%s" $(pwd) $2 > $HOME/.cache/fastc.save
-		gcc -Wall -o C $2 && ./C;;
+	"config") sed -i "/$(pwd)/d" $HOME/.cache/fastc.save
+		printf "%s\t%s\n" "$(pwd)" "$save" >> $HOME/.cache/fastc.save
+		gcc -Wall -o C $save && ./C;;
 	*) echo "Usage: fastc.sh [OPTION] [SOURCEFILE]
                 A simple script for faster C script management.
 
